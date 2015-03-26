67c23aabd53482699d052a84605aa391d8cfe5bd
nemumu <git@nemumu.com>
2015-03-27 00:56:58 +0900

    設定ファイルを追加

---
+++ main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+#=============================
+#       PukiWikiの設定
+#=============================
+# PukiWikiトップページのURL
+PUKIWIKI_URL = ""
+
+# PukiWikiのページ閲覧にBasic認証が有るか
+# (Trueなら有り、Falseなら無し)
+PUKIWIKI_BASIC = True
+
+#=============================
+#       MediaWikiの設定
+#=============================
+# MediaWikiトップページのURL
+MEDIAWIKI_URL = ""
+
+# MediaWikiのログインID
+MEDIAWIKI_LOGIN_ID = ""
+
+# MediaWikiのログインPW
+MEDIAWIKI_LOGIN_PW = ""
+
+#=============================
+#       その他設定
+#=============================
+# デバッグモードかどうか
+# (Trueならデバッグモード)
+# ライブラリを編集するときに使うと良いかも
+OTHER_DEBUG_MODE = True
+
+

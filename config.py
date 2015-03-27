2597bea340278a30c39f81bfb031517b9396be57
nemumu <git@nemumu.com>
2015-03-27 21:50:05 +0900

    設定ファイルを分割

---
+++ config.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from pukiwiki_converter import *
+
+#=============================
+#       PukiWikiの設定
+#=============================
+# PukiWikiトップページのURL
+PUKIWIKI_URL = ""
+
+# PukiWikiのページにBasic認証が有るか
+# (Trueなら有り、Falseなら無し)
+PUKIWIKI_BASIC = True
+
+# PukiWikiページのBasic認証ID
+# (PUKIWIKI_BASICがTrueの場合のみ使用)
+PUKIWIKI_BASIC_ID = ""
+
+# PukiWikiページのBasic認証PW
+# (PUKIWIKI_BASICがTrueの場合のみ使用)
+PUKIWIKI_BASIC_PW = ""
+
+#=============================
+#       MediaWikiの設定
+#=============================
+# MediaWikiトップページのURL
+MEDIAWIKI_URL = ""
+
+# MediaWikiのログインID
+MEDIAWIKI_LOGIN_ID = ""
+
+# MediaWikiのログインPW
+MEDIAWIKI_LOGIN_PW = ""
+
+#=============================
+#       その他設定
+#=============================
+# デバッグモードかどうか
+# (Trueならデバッグモード)
+# ライブラリを編集するときに使うと良いかも
+OTHER_DEBUG_MODE = True
--- main.py
+++ main.py
@@ -1,39 +1,6 @@
 # -*- coding: utf-8 -*-
 from pukiwiki_converter import *
-
-#=============================
-#       PukiWikiの設定
-#=============================
-# PukiWikiトップページのURL
-PUKIWIKI_URL = ""
-
-# PukiWikiのページ閲覧にBasic認証が有るか
-# (Trueなら有り、Falseなら無し)
-PUKIWIKI_BASIC = True
-
-#=============================
-#       MediaWikiの設定
-#=============================
-# MediaWikiトップページのURL
-MEDIAWIKI_URL = ""
-
-# MediaWikiのログインID
-MEDIAWIKI_LOGIN_ID = ""
-
-# MediaWikiのログインPW
-MEDIAWIKI_LOGIN_PW = ""
-
-#=============================
-#       その他設定
-#=============================
-# デバッグモードかどうか
-# (Trueならデバッグモード)
-# ライブラリを編集するときに使うと良いかも
-OTHER_DEBUG_MODE = True
-
-#=============================
-#    ライブラリ呼び出し処理
-#=============================
+from config import *
 
 if __name__ == "__main__":
 	# PukiWikiからデータの取り出し

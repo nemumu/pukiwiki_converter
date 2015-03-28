0f71eb1e1ed564641339203cbaf323794e51737d
nemumu <git@nemumu.com>
2015-03-28 20:48:29 +0900

    ファイル名の取得処理を追加

--- pukiwiki_converter.py
+++ pukiwiki_converter.py
@@ -2,6 +2,9 @@
 
 import requests
 import re
+import time
+import urllib
+import os
 
 #=====================================
 # PukiWikiからMediaWikiへ変換する処理
@@ -18,7 +21,7 @@ class pukiwiki_convert:
 		self.mediawiki_login_id  = mediawiki_login_id
 		self.mediawiki_login_pw  = mediawiki_login_pw
 		self.other_debug_mode    = other_debug_mode
-		self.data_cache_directory= data_cache_directory
+		self.data_cache_directory= data_cache_directory + '/'
 		
 		print "__init__"
 	
@@ -29,10 +32,13 @@ class pukiwiki_convert:
 		sess = requests.session()
 
 		# Basic認証が必要な場合はフィールドを追加
+		response = self.__get_data(page_list_url)
+		'''
 		if self.pukiwiki_basic == True:
 			response = sess.get(page_list_url, auth=(self.pukiwiki_basic_id, self.pukiwiki_basic_pw))
 		else:
 			response = sess.get(page_list_url)
+		'''
 
 		html = response.text
 
@@ -48,14 +54,84 @@ class pukiwiki_convert:
 		
 		return matchedList
 
-	# PukiWikiから指定URLのページを取得
+	# PukiWikiから指定ページIDのページを取得
 	# 添付ファイルも含めてディレクトリに保存
-	def get_page(self):
+	def get_page(self, page_id):
+		# ディレクトリを作成
+		try:
+			os.makedirs(self.data_cache_directory + page_id)
+		except OSError:
+			print "Duplicate File"
+
+		# 添付ファイルをダウンロード
+		self.__get_page_attach(page_id)
+
+		# テキストをダウンロード
+
 		print "get_page"
 	
-	# PukiWikiのURLから指定ディレクトリ内に添付ファイルを保存
-	def __get_attach(self, wiki_url, directory):
+	# PukiWikiの記事IDからディレクトリ内に添付ファイルを保存
+	def __get_page_attach(self, page_id):
+		# ページからダウンロードリンク一覧を取得
+		page_url = self.pukiwiki_url + 'index.php?' + page_id
+		sess = requests.session()
+
+		response = self.__get_data(page_url)
+
+		html = response.text
+
+		file_list = re.findall(self.pukiwiki_url + r'index.php\?plugin=attach&amp;pcmd=open&amp;file=.+?"', html)
+
+		for i in range( len(file_list) ):
+			file_list[i] = file_list[i].replace('"', '')
+			file_list[i] = file_list[i].replace('amp;', '&')
+
+			#print file_list[i]
+
+			# ファイル名を取り出す
+			file_name = ""
+			file_name_regex = re.compile('&file=.+?&')
+			file_name_match = file_name_regex.search( file_list[i] )
+
+			if file_name_match:
+				file_name = file_name_match.group()
+				file_name = file_name.replace('&file=', '')
+				file_name = file_name.replace('&', '')
+				file_name = urllib.unquote( file_name ).decode('utf-8')
+
+			print "File_name:%s" %  file_name
+				
+			continue
+
+			# ダウンロードしてディレクトリに保存
+			response = self.__get_data(file_list[i])
+
+			# ファイルのダウンロードに成功したら保存する
+			if response.status_code == 200:
+				print "Save data"
+				
+
+			break
+
+			
+
+		# ダウンロードしてディレクトリに保存
 		print "get_attach"
+	
+	# PukiWikiの記事IDからディレクトリ内にテキストを保存
+	# (この時点ではMediaWikiフォーマットへの変換は行わない)
+	def __get_page_text(self, page_id):
+		print "get_page_text"
+	
+	# GETでデータを取得するメソッド
+	def __get_data(self, url):
+		sess = requests.session()
+
+		if self.pukiwiki_basic == True:
+			response = sess.get(url, auth=(self.pukiwiki_basic_id, self.pukiwiki_basic_pw) )
+		else:
+			response = sess.get(url)
+		return response
 	
 	# PukiWikiの形式からMediaWikiの形式
 	def __p2m_convert(self):
@@ -64,3 +140,4 @@ class pukiwiki_convert:
 	# データをMediaWikiにアップロード
 	def mediawiki_upload(self):
 		print "mediawiki_upload"
+	
